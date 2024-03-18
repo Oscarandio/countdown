d72941094854a5c4d68a91fe6a37f1adee391be1
Óscar Rabadán Martínez <rabadanoscar@outlook.es>
2024-03-18 20:52:16 +0100

    typescript

--- countdown-ts/src/main.ts
+++ countdown-ts/src/main.ts
@@ -3,43 +3,54 @@ const MINUTE: number = SECOND * 60;
 const HOUR: number = MINUTE * 60;
 const DAY: number = HOUR * 24;
 
+const getElement = <T extends HTMLSpanElement>(selector: string): T | null => {
+  return document.querySelector<T>(selector);
+};
+
+const updateCountdown = (
+  days: HTMLElement | null,
+  hours: HTMLElement | null,
+  minutes: HTMLElement | null,
+  seconds: HTMLElement | null,
+  date: number
+) => {
+  const now = Date.now();
+  const diff = date - now;
+
+  if (days) {
+    days.innerText = Math.floor(diff / DAY).toString();
+  }
+
+  if (hours) {
+    hours.innerText = Math.floor((diff % DAY) / HOUR).toString();
+  }
+
+  if (minutes) {
+    minutes.innerText = Math.floor((diff % HOUR) / MINUTE).toString();
+  }
+
+  if (seconds) {
+    seconds.innerText = Math.floor((diff % MINUTE) / SECOND).toString();
+  }
+};
+
 const init = () => {
-  const $countdown = document.querySelector('[data-date]');
+  const $countdown = getElement<HTMLElement>('[data-date]');
   if (!$countdown) return;
 
-  const $days = $countdown.querySelector('[data-days]');
-  const $hours = $countdown.querySelector('[data-hours]');
-  const $minutes = $countdown.querySelector('[data-minutes]');
-  const $seconds = $countdown.querySelector('[data-seconds]');
+  const $days = getElement<HTMLElement>('[data-days]');
+  const $hours = getElement<HTMLElement>('[data-hours]');
+  const $minutes = getElement<HTMLElement>('[data-minutes]');
+  const $seconds = getElement<HTMLElement>('[data-seconds]');
 
   const timestamp = $countdown.getAttribute('data-date');
   if (!timestamp) return;
 
   const date = new Date(+timestamp).getTime();
 
-  const updateCountdown = () => {
-    const now = Date.now();
-    const diff = date - now;
-
-    if ($days instanceof HTMLElement) {
-      $days.innerText = Math.floor(diff / DAY).toString();
-    }
-
-    if ($hours instanceof HTMLElement) {
-      $hours.innerText = Math.floor((diff % DAY) / HOUR).toString();
-    }
-
-    if ($minutes instanceof HTMLElement) {
-      $minutes.innerText = Math.floor((diff % HOUR) / MINUTE).toString();
-    }
-
-    if ($seconds instanceof HTMLElement) {
-      $seconds.innerText = Math.floor((diff % MINUTE) / SECOND).toString();
-    }
-  }
-
-  setInterval(updateCountdown, SECOND);
-}
+  setInterval(() => {
+    updateCountdown($days, $hours, $minutes, $seconds, date);
+  }, SECOND);
+};
 
 init();
-
